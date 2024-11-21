d32dea125f9c1c2ce43fc92faf57675264c5ec5c
Ravindupereraofficial <ravindurandika2004@gmail.com>
2024-11-21 09:57:29 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+function sender1(){
+    console.log("al");
+}
+function sender2(){
+    console.log("alu");
+}
+
+function sendmessege(){
+
+    let sender11;
+    let sender22;
+
+    let dropdwn1=document.getElementById("sender1").value;
+    let dropdwn2=document.getElementById("sender2").value;
+    console.log(dropdwn1);
+    
+    let usermg=document.getElementById("gettext").value="";
+    console.log(usermg);
+}
